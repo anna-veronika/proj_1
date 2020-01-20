20da2ce7ef0c1efced8a8769abbaca6abc86e217
anna-veronika <anna-veronika@users.noreply.github.com>
2020-01-20 11:50:43 +0100

    blast

--- hello.R
+++ hello.R
@@ -5,3 +5,34 @@
 
 print('hello_r', quote = FALSE)
 
+blastn = "/path/to/blast/ncbi-blast-2.6.0+/bin/blastn"
+blast_db = "blast_database"
+input = "input_file"
+evalue = 1e-6
+format = 6
+colnames <- c("qseqid",
+               "sseqid",
+               "pident",
+               "length",
+               "mismatch",
+               "gapopen",
+               "qstart",
+               "qend",
+               "sstart",
+               "send",
+               "evalue",
+               "bitscore")
+
+blast_out <- system2(command = "blastn", 
+                     args = c("-db", blast_db, 
+                              "-query", input, 
+                              "-outfmt", format, 
+                              "-evalue", evalue,
+                              "-ungapped"),
+                     wait = TRUE,
+                     stdout = TRUE) %>%
+  as_tibble() %>% 
+  separate(col = value, 
+           into = colnames,
+           sep = "\t",
+           convert = TRUE)
